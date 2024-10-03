1a3646ba115fd61c5b52d27bba557a9130b6ef42
Benjamin Mundama <benjamin.mundama@strathmore.edu>
2024-10-03 14:28:14 +0300

    cretaed a verification code attribute in the users' table

--- includes/constants.php
+++ includes/constants.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set("AFRICA/Nairobi");
+
 define('DBTYPE', 'PDO');
 define('HOSTNAME', 'localhost');
 define('DBPORT', '3306');
@@ -11,6 +13,7 @@ $protocol = isset($_SERVER['HTTPS']) &&
 $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
 $base_url = $protocol . $_SERVER['HTTP_HOST'] . '/';
 
+$conf['ver_code_time'] = date("Y-m-d H:i:s", strtotime("+ 1 days"));
 $conf['verification_code'] = rand(10000,99999);
 $conf['site_initials'] = "ICS 2024";
 $conf['site_url'] = "$base_url/". DBNAME;
