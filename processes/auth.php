206fbea2068fe306d1da86daf3a89edf9b3b4149
Benjamin Mundama <benjamin.mundama@strathmore.edu>
2024-10-03 11:23:03 +0300

    Implemented various validations in the auth.php file

--- processes/auth.php
+++ processes/auth.php
@@ -1,55 +1,99 @@
 <?php
 class auth{
-    public function signup($conn, $ObjGlob){
+
+    public function bind_to_template($replacements, $template) {
+        return preg_replace_callback('/{{(.+?)}}/', function($matches) use ($replacements) {
+            return $replacements[$matches[1]];
+        }, $template);
+    }
+
+    public function signup($conn, $ObjGlob, $ObjSendMail, $lang, $conf){
         if(isset($_POST["signup"])){
 
             $errors = array();
 
-            $fullname = $_POST["fullname"];
-            $email_address = $_POST["email_address"];
-            $username = $_POST["username"];
+            $fullname = $_SESSION["fullname"] = $conn->escape_values(ucwords(strtolower($_POST["fullname"])));
+            $email_address = $_SESSION["email_address"] = $conn->escape_values(strtolower($_POST["email_address"]));
+            $username = $_SESSION["username"] = $conn->escape_values(strtolower($_POST["username"]));
 
 // Implement input validation and error handling
-//=================================================
+// =============================================
+// Sanitize all inputs
 
+// verify that the fullname has only letters, space, dash, quotation
+if(ctype_alpha(str_replace(" ", "", str_replace("\'", "", $fullname))) === FALSE){
+    $errors['nameLetters_err'] = "Invalid name format: Full name must contain letters and spaces only etc " . $fullname;
+}
 
-// verify the the fullname only contains letter, space, and an quotation sign
-
-// verify that the email has the correct format
-
+// verify that the email has got the correct format
 if(!filter_var($email_address, FILTER_VALIDATE_EMAIL)){
     $errors['email_format_err'] = 'Wrong email format';
 }
 
-// verify that the email address domain is authorized (@strathmore.edu, gmail.com) not (@yanky.sco)
-// verify that the new email address does not exist already in the database
+// verify that the email domain is authorized (@strathmore.edu, @gmail.com, @yahoo.com, @mada.co.ke) and not (@yanky.net)
+$conf['valid_domains'] = ["strathmore.edu", "gmail.com", "yahoo.com", "mada.co.ke", "outlook.com", "STRATHMORE.EDU", "GMAIL.COM", "YAHOO.COM", "MADA.CO.KE", "OUTLOOK.COM"];
 
-// verify that the new username does not exist already in the database
+$arr_email_address = explode("@", $email_address);
+$spot_dom = end($arr_email_address);
+$spot_user = reset($arr_email_address);
 
-// Implement 2FA (email == PHP-Mailer).
-//=====================================
+if(!in_array($spot_dom, $conf['valid_domains'])){
+    $errors['mailDomain_err'] = "Invalid email address domain. Use only: " . implode(", ", $conf['valid_domains']);
+}
+$exist_count = 0;
+// Verify Email Already Exists
+$spot_email_address_res = $conn->count_results(sprintf("SELECT email FROM users WHERE email = '%s' LIMIT 1", $email_address));
+if ($spot_email_address_res > $exist_count){
+    $errors['mailExists_err'] = "Email Already Exists";
+}
 
-// verify that the password is identical to the repeat password
+// Verify Username Already Exists
+$spot_username_res = $conn->count_results(sprintf("SELECT username FROM users WHERE username = '%s' LIMIT 1", $username));
+if ($spot_username_res > $exist_count){
+    $errors['usernameExists_err'] = "Username Already Exists";
+}
 
-// verify that the password length is between 4 and 8 characters 
+// Verify if username contain letters only
+if (!ctype_alpha($username)) {
+    $errors['usernameLetters_err'] = "Invalid username format. Username must contain letters only";
+    $ObjGlob->setMsg('errors', $errors, 'invalid');
+}
 
+// Verify that the password is identical to the repeat passsword
+// verify that the password length is between 4 and 8 characters
 if(!count($errors)){
-    $cols = ['fullname', 'email', 'username'];
-    $vals = [$fullname, $email_address, $username];
 
-    $data = array_combine($cols, $vals);
+// Implement 2FA (email => PHP-Mailer)
+// ===================================
+// Send email verification with an OTP (OTC)
 
-    $insert = $conn->insert('users', $data);
-        if($insert === TRUE){
-            header('Location: signup.php');
-            exit();
+
+            $cols = ['fullname', 'email', 'username'];
+            $vals = [$fullname, $email_address, $username];
+            $data = array_combine($cols, $vals);
+            $insert = $conn->insert('users', $data);
+            if($insert === TRUE){
+
+                $replacements = array('fullname' => $fullname, 'email_address' =>
+                $email_address, 'verification_code' => $conf['verification_code'], 'site_full_name' => strtoupper($conf['site_initials']));
+        
+                $ObjSendMail->SendMail([
+                    'to_name' => $fullname,
+                    'to_email' => $email_address,
+                    'subject' => $this->bind_to_template($replacements, $lang["AccountVerification"]),
+                    'message' => $this->bind_to_template($replacements, $lang["AccRegVer_template"])
+                ]);
+                
+                header('Location: signup.php');
+                unset($_SESSION["fullname"], $_SESSION["email_address"], $_SESSION["username"]);
+                exit();
+            }else{
+                die($insert);
+            }
         }else{
-            die($insert);
+            $ObjGlob->setMsg('msg', 'Error(s)', 'invalid');
+            $ObjGlob->setMsg('errors', $errors, 'invalid');
         }
-    }else{
-        $ObjGlob->setMsg('msg', 'Error(s)', 'invalid-feedback');
-        $ObjGlob->setMsg('errors', $errors, 'invalid-feedback');
     }
-        }
     }
 }
